f54e5d336592e08fe4878c71bf39eb1c7269cebe
Rui Chen <chenruix47@gmail.com>
2022-05-03 00:26:01 -0400

    added view that displays html for all entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 
 
@@ -7,4 +7,9 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entries(request, entry):
+    if util.get_entry(entry)==None:
+        return HttpResponse("Entry not found")
+    return render(request, "encyclopedia/entries.html",{"entry":entry,"content":util.get_entry(entry)})
 
