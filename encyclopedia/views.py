902131f28ae7829e5114e3afaca73f3ea8977779
Rui Chen <chenruix47@gmail.com>
2022-05-03 15:14:25 -0400

    gave related search result links if the input entry does not exist

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,16 +9,18 @@ class SearchForm(forms.Form):
 
 def index(request):
     if request.method=="POST":
-        form=SearchForm(request.POST)
-        if form.is_valid():
-            search=form.cleaned_data["search"]
-            return HttpResponseRedirect(search)
+        search=request.POST["q"]
+        return HttpResponseRedirect(search)
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),"form":SearchForm()
     })
 
 def entries(request, entry):
     if entry not in util.list_entries():
-        return HttpResponse("Entry not found")
-    return render(request, "encyclopedia/entries.html",{"entry":entry,"content":util.get_entry(entry),"form":SearchForm()})
+        search=[]
+        for i in util.list_entries():
+            if entry.casefold() in i.casefold():
+                search.append(i)
+        return render(request, "encyclopedia/searchresult.html",{"entry":entry,"search":search})
+    return render(request, "encyclopedia/entries.html",{"entry":entry,"content":util.get_entry(entry)})
 
