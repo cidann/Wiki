536f7a4d2f3762d09f18524b1921203b917bdbac
Rui Chen <chenruix47@gmail.com>
2022-05-03 13:17:41 -0400

    added search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 from . import util
 
+class SearchForm(forms.Form):
+    search=forms.CharField(label="")
 
 def index(request):
+    if request.method=="POST":
+        form=SearchForm(request.POST)
+        if form.is_valid():
+            search=form.cleaned_data["search"]
+            return HttpResponseRedirect(search)
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),"form":SearchForm()
     })
 
 def entries(request, entry):
-    if util.get_entry(entry)==None:
+    if entry not in util.list_entries():
         return HttpResponse("Entry not found")
-    return render(request, "encyclopedia/entries.html",{"entry":entry,"content":util.get_entry(entry)})
+    return render(request, "encyclopedia/entries.html",{"entry":entry,"content":util.get_entry(entry),"form":SearchForm()})
 
